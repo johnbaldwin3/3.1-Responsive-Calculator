d70df2f52dc6e08905539c761b64d91b723acf48
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-01-22 11:36:25 -0500

    styling done, click colors done

--- main.js
+++ main.js
@@ -2,16 +2,14 @@
   "use strict";
   console.log("stuff");
 
-var buttonClicked = document.getElementsByClassName('bttn');
 
-function buttonColorClick(){
+var display = document.getElementById('calc-display');
+var b1 = document.getElementById('one');
+var b2 = document.getElementById('two');
+var b3 = document.getElementById('three');
+var b4 = document.getElementById('four');
+var b5 = document.getElementById('five');
 
-  buttonClicked.addEventListener('click', function(){
-    console.log("clicked");
-    buttonClicked.style.backgroundColor = 'grey';
-  })
-
-}
 
 
 

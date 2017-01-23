452bc22e047f2c9bab83273ab194bf6c8632068a
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-01-23 17:28:29 -0500

    some working addition

--- main.js
+++ main.js
@@ -4,12 +4,60 @@
 
 
 var display = document.getElementById('calc-display');
-var b1 = document.getElementById('one');
-var b2 = document.getElementById('two');
-var b3 = document.getElementById('three');
-var b4 = document.getElementById('four');
-var b5 = document.getElementById('five');
+var numberPressed = document.querySelectorAll('.number');
+var operatorPressed = document.querySelectorAll('.operators');
+var pushNumber;
+var pushOperator;
 
+var result;
+var input = 0;
+
+var resetInput = true;
+
+
+
+for (var i = 0; i < numberPressed.length; i++) {
+  numberPressed[i].addEventListener('click', function(event){
+  pushNumber= event.target.dataset.value;
+  // console.log(display.textContent);
+  if (resetInput) {
+    input = pushNumber
+    display.textContent = input;
+    resetInput = false;
+  } else {
+    input += pushNumber
+    display.textContent = input;
+  }
+  // console.log(pushNumber);
+  })
+}
+
+for (var i=0; i < operatorPressed.length; i++) {
+  operatorPressed[i].addEventListener('click', function(event){
+    var pushOperator;
+
+    if (result === undefined) {
+      result = input;
+      resetInput = true;
+      pushOperator = event.target.dataset.value;
+    } else {
+      var test = Number(result) + Number(input);
+      console.log(test);
+    }
+
+  })
+}
+
+
+function allClear() {
+
+  if (operatorPressed == 'AC') {
+    display.textContent = '0';
+  }
+};
+// numberPressed.addEventListener('click', function(event){
+//   var myVal = Number(event.target.getAttribute('value'));
+//   console.log(myVal);
 
 
 
